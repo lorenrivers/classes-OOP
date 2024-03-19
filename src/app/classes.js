79b6ceec0f49952f27af06e0c14c8d89c5ae59fc
Loren Rivers <loren.rivers@hotmail.co.uk>
2024-03-19 12:01:18 +0000

    classes and inheritance workshop

---
+++ src/app/classes.js
@@ -0,0 +1,57 @@
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(`${this.name} makes a noise.`);
+  }
+  poop() {
+    console.log(`${this.name} poops.`);
+  }
+}
+
+const animal = new Animal("Dog");
+animal.speak();
+animal.poop();
+
+class Dog extends Animal {
+  speak() {
+    console.log(`${this.name} barks.`);
+  }
+}
+
+const dog = new Dog("Hooch");
+dog.speak();
+dog.poop();
+
+class Bird extends Animal {
+  speak() {
+    console.log(`${this.name} chirps.`);
+  }
+
+  fly() {
+    console.log(`${this.name} flies.`);
+  }
+}
+
+const bird = new Bird("Sparrow");
+bird.speak();
+bird.poop();
+bird.fly();
+
+class FlyingAnimal extends Animal {
+  fly() {
+    console.log(`${this.name} flies.`);
+  }
+}
+
+class Pig extends FlyingAnimal {
+  speak() {
+    console.log(`${this.name} oinks.`);
+  }
+}
+
+const pig = new Pig("Porky");
+pig.speak();
+pig.poop();
+pig.fly();
---
+++ src/app/weaponClasses.js
@@ -0,0 +1,39 @@
+class Weapon {
+  constructor(item, damage) {
+    this.item = item;
+    this.damage = damage;
+  }
+  attack() {
+    console.log(
+      `You have been attacked with ${this.item}! Damage of ${this.damage} has been inflicted.`
+    );
+  }
+}
+
+class Sword extends Weapon {
+  slice() {
+    console.log(`${this.item} sliced you!`);
+  }
+  poke() {
+    console.log(`${this.item} poked you!`);
+  }
+}
+
+const oathkeeper = new Weapon("OathKeeper", 10);
+oathkeeper.attack();
+
+const longclaw = new Sword("Longclaw");
+// longclaw.poke();
+
+class Armour {
+  constructor(item, defence) {
+    this.item = item;
+    this.defence = defence;
+  }
+  block() {
+    console.log(`A ${this.item} was used to block you!`);
+  }
+}
+
+const shield = new Armour("shield");
+// shield.block();
